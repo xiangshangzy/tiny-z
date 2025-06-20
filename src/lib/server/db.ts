6ff1b30de722af4888d66f7ab5301c5db5a9c59a
13410 <13410704085@163.com>
2025-06-20 20:20:01 +0800

    test neon serveless

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,5 +1,7 @@
-import { drizzle } from "drizzle-orm/node-postgres";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
 import { DATABASE_URL } from "$env/static/private";
 if (typeof DATABASE_URL === 'undefined')
   throw new Error('DATABASE Environment variable is not set')
-export const db = drizzle(DATABASE_URL);
+const sql = neon(DATABASE_URL)
+export const db = drizzle(sql);
